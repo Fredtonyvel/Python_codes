fbe5ae92d58654daff86bdb28c3e69347b140d36
torabey <34867547+torabey@users.noreply.github.com>
2018-10-26 15:39:42 +0300

    Update Python Lecture 2.6 problem set code.py

--- Python Lecture 2.6 problem set code.py
+++ Python Lecture 2.6 problem set code.py
@@ -1,45 +1,8 @@
-if 6 > 7:
-    print "Yep"
-else:
-    print "Nope"
-   
-    
-      
-var = 'Panda'
-if var == "panda":
-   print "Cute!"
-elif var == "Panda":
-   print "Regal!"
-else:
-   print "Ugly..."
-   
-   
-   
-temp = '32'
-if temp > 85:
-   print "Hot"
-elif temp > 62:
-   print "Comfortable" 
-else:
-   print "Cold" 
-   
-   
-temp = 120
-if temp > 85:
-   print "Hot"
-elif temp > 100:
-   print "REALLY HOT!"
-elif temp > 60:
-   print "Comfortable" 
-else:
-   print "Cold"
-   
-   
-happy = 3
-if happy > 2:
-    print "hello world"
-    
-    
+ print "Nope"
+ print "Cute!"
+ print "Cold" 
+ print "Hot"
+ print "hello world"
 varA = 'abcg'
 varB = 'def'
 if (type(varA) or type(varB)) == str:
