e1d8c897a373d15886d8152371aeb01c0ad3b01a
torabey <34867547+torabey@users.noreply.github.com>
2018-10-26 15:43:05 +0300

    Update Python Lecture 3 simple algorithms.py

--- Python Lecture 3 simple algorithms.py
+++ Python Lecture 3 simple algorithms.py
@@ -2,31 +2,22 @@ num = 0
 while num <= 5:
     print num
     num += 1
-
-print "Outside of loop"
+"""print "Outside of loop"
 print num 
-
-
 print "\n\n"
-
-
-#infinite loop
-#numberOfLoops = 0
-#numberOfApples = 2
-#while numberOfLoops < 10:
- #   numberOfApples *= 2
- #  numberOfApples += numberOfLoops
- #   numberOfLoops -= 1
-#print "Number of apples: " + str(numberOfApples)
-
-
+infinite loop
+numberOfLoops = 0
+numberOfApples = 2
+while numberOfLoops < 10:
+   numberOfApples *= 2
+   numberOfApples += numberOfLoops
+   numberOfLoops -= 1
+print "Number of apples: " + str(numberOfApples)"""
 num = 10
 while num > 3:
     num -= 1
     print num 
-
 print "\n\n"    
-
 num = 10
 while True:
     if num < 7:
@@ -35,32 +26,20 @@ while True:
     print num
     num -= 1
 print 'Outside of loop'   
-
-      
 print "\n\n"
-
-
-#infinite loop                                
-#num = 100
-#while not False:
- #   if num < 0:
- #       break
-#print 'num is: ' + str(num)    
-
-                                                   
+"""infinite loop                                
+num = 100
+while not False:
+    if num < 0:
+        break
+print 'num is: ' + str(num)    """                                                
 print "\n\n"
-
-
 num = 0
 while num < 10:
     num += 2
     print num
 print 'Goodbye!'
-
-
 print "\n\n"
-
-
 num = 12
 print 'Hello!'
 while num > 2:
@@ -71,10 +50,8 @@ num = 10
 print 'Hello!'
 while num > 0:
     print num
-    num -= 2
-    
+    num -= 2   
 print "\n\n"
-
 total = 0
 num = 1
 end = 6
